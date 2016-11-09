5362eb2df1242b055e8aeb873f97c95275986969
ejanjoh <ejanjoh@Jans-MacBook-Air.local>
2016-11-09 23:27:54 +0100

    ver 4

--- esp210/make_esp210.mk
+++ esp210/make_esp210.mk
@@ -3,7 +3,7 @@
 #       Autor:      Jan Johansson (ejanjoh)
 #       Copyright:  Copyright (c) 2016 Jan Johansson (ejanjoh), All rights reserved.
 #       Created:    2016-11-04
-#       Updated:    
+#       Updated:    2016-11-09
 #
 #       Project:    makefile esp210
 #       File name:  make_esp210.mk
@@ -13,6 +13,8 @@
 #       -----------------------------------------------
 #       ver 1:      Created
 #       ver 3:      Corrected typo
+#       ver 4:      Added hooks for project related core files.
+#                   Changed how the path separator order "/" are used in variables
 #
 #       Reference:  https://github.com/arduino/Arduino/wiki/Arduino-IDE-1.5-3rd-party-Hardware-specification
 #                   https://github.com/esp8266/Arduino
@@ -37,21 +39,22 @@
 
 
 # User defined, global predefined or by the ide "normaly" defined properties
-TOOLS_PATH                  := <your path e.g. .../test>/Arduino15/packages/esp8266/tools/
-ARDUINO_PATH                := <your path e.g. .../test>/
-PROJ_PATH                   := <your path e.g. .../test/sketch_dir>
+PROJ_ROOT                   := < add your path e.g. .../physical_device/esp210>
+TOOLS_PATH                  := $(PROJ_ROOT)/Arduino15/packages/esp8266/tools
+ARDUINO_PATH                := $(PROJ_ROOT)
+PROJ_PATH                   := $(PROJ_ROOT)/source
 
-RUNTIME_PLATFORM_PATH       := $(ARDUINO_PATH)Arduino15/packages/esp8266/hardware/esp8266/2.3.0/
-RUNTIME_TOOLS_XTENSA-LX106-ELF-GCC_PATH := $(TOOLS_PATH)xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2/
-RUNTIME_IDE_VERSION         := 10612
-RUNTIME_TOOLS_ESPTOOL_PATH  := $(TOOLS_PATH)esptool/0.4.9/
+RUNTIME_PLATFORM_PATH                   := $(ARDUINO_PATH)/Arduino15/packages/esp8266/hardware/esp8266/2.3.0
+RUNTIME_TOOLS_XTENSA-LX106-ELF-GCC_PATH := $(TOOLS_PATH)/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2
+RUNTIME_IDE_VERSION                     := 10612
+RUNTIME_TOOLS_ESPTOOL_PATH              := $(TOOLS_PATH)/esptool/0.4.9
 
-BUILD_PATH                  := <your path e.g. .../test/build>
+BUILD_PATH                  := $(PROJ_ROOT)/build
 BUILD_F_CPU                 := 80000000L
 BUILD_BOARD                 := ESP8266_ESP210
 BUILD_ARCH                  := ESP8266
-BUILD_CORE_PATH             := $(ARDUINO_PATH)Arduino15/packages/esp8266/hardware/esp8266/2.3.0/cores/esp8266/
-BUILD_VARIANT_PATH          := $(RUNTIME_PLATFORM_PATH)variants/generic/
+BUILD_CORE_PATH             := $(ARDUINO_PATH)/Arduino15/packages/esp8266/hardware/esp8266/2.3.0/cores/esp8266
+BUILD_VARIANT_PATH          := $(RUNTIME_PLATFORM_PATH)/variants/generic
 BUILD_PROJECT_NAME          := test
 BUILD_FLASH_LD              := eagle.flash.4m.ld
 BUILD_FLASH_MODE            := qio
@@ -64,10 +67,23 @@ UPLOAD_SPEED                := 115200
 SERIAL_PORT                 := /dev/cu.SLAB_USBtoUART
 
 
+# ----- NOTE: PROJECT SPECIFIC SYSTEM INCLUDES -----
+
+PROJ_SPEC_SYS_INC           := 
+PROJ_SPEC_SYS_SRC           := 
+
+# e.g. if you would use WiFi and send datagram over UDP
+#PROJ_SPEC_SYS_INC           := $(shell find $(RUNTIME_PLATFORM_PATH)/libraries/ESP8266WiFi/src -name "*.h")
+#PROJ_SPEC_SYS_SRC           := $(RUNTIME_PLATFORM_PATH)/libraries/ESP8266WiFi/src/
+
+# --------------------------------------------------
+
+
 # Includes
 INCLUDE                     := $(shell find $(PROJ_PATH) -name "*.h")
 INCLUDE                     += $(shell find $(BUILD_CORE_PATH) -name "*.h")
 INCLUDE                     += $(shell find $(BUILD_VARIANT_PATH) -name "*.h")
+INCLUDE                     += $(PROJ_SPEC_SYS_INC)
 INCLUDE_PATHS               := $(sort $(dir $(INCLUDE)))
 INCLUDES                    := $(foreach dir, $(INCLUDE_PATHS), -I $(dir))
 
@@ -75,11 +91,12 @@ INCLUDES                    := $(foreach dir, $(INCLUDE_PATHS), -I $(dir))
 # Source
 TEMP_PROJ_PATHS             := $(shell find $(PROJ_PATH) -type d)
 TEMP_CORE_PATHS             := $(shell find $(BUILD_CORE_PATH) -type d)
+TEMP_CORE_PATHS             += $(PROJ_SPEC_SYS_SRC)
 VPATH                       := $(TEMP_PROJ_PATHS) 
 VPATH                       += $(TEMP_CORE_PATHS)
 
 
-# Objects - Note, the linker script used in this environment need the source
+# Objects - Note, the linker script found at "https://github.com/esp8266/Arduino/..." need the source
 # file extension before the object file extension. In addition ".ino" ---> ".cpp.o"
 OBJECT_FILES    := $(foreach dir, $(VPATH), $(patsubst $(dir)/%.S, $(BUILD_PATH)/%.S.o, $(wildcard $(dir)/*.S)))
 OBJECT_FILES    += $(foreach dir, $(VPATH), $(patsubst $(dir)/%.c, $(BUILD_PATH)/%.c.o, $(wildcard $(dir)/*.c)))
@@ -97,8 +114,8 @@ CORE_OBJ_FILES  += $(foreach dir, $(TEMP_CORE_PATHS), $(patsubst $(dir)/%.cpp, $
 
 
 # Tools paths
-COMPILER_PATH               := $(RUNTIME_TOOLS_XTENSA-LX106-ELF-GCC_PATH)/bin/
-COMPILER_SDK_PATH           := $(RUNTIME_PLATFORM_PATH)tools/sdk/
+COMPILER_PATH               := $(RUNTIME_TOOLS_XTENSA-LX106-ELF-GCC_PATH)/bin
+COMPILER_SDK_PATH           := $(RUNTIME_PLATFORM_PATH)/tools/sdk
 PATH                        := $(RUNTIME_TOOLS_ESPTOOL_PATH)
 
 
@@ -148,71 +165,71 @@ COMPILER_C_ELF_LIBS             += -lsmartconfig -lmesh -lwpa2 $(BUILD_LWIP_LIB)
 
 
 # Recipes
-RECIPE_S_O_PATTERN          := $(COMPILER_PATH)$(COMPILER_C_CMD) $(COMPILER_CPREPROCESSOR_FLAGS) $(COMPILER_S_FLAGS)
+RECIPE_S_O_PATTERN          := $(COMPILER_PATH)/$(COMPILER_C_CMD) $(COMPILER_CPREPROCESSOR_FLAGS) $(COMPILER_S_FLAGS)
 RECIPE_S_O_PATTERN          += -DF_CPU=$(BUILD_F_CPU) $(BUILD_LWIP_FLAGS) -DARDUINO=$(RUNTIME_IDE_VERSION)
 RECIPE_S_O_PATTERN          += -DARDUINO_$(BUILD_BOARD) -DARDUINO_ARCH_$(BUILD_ARCH) -DARDUINO_BOARD=$(BUILD_BOARD)
 RECIPE_S_O_PATTERN          += $(INCLUDES)
 
-RECIPE_C_O_PATTERN          := $(COMPILER_PATH)$(COMPILER_C_CMD) $(COMPILER_CPREPROCESSOR_FLAGS) $(COMPILER_C_FLAGS)
+RECIPE_C_O_PATTERN          := $(COMPILER_PATH)/$(COMPILER_C_CMD) $(COMPILER_CPREPROCESSOR_FLAGS) $(COMPILER_C_FLAGS)
 RECIPE_C_O_PATTERN          += -DF_CPU=$(BUILD_F_CPU) $(BUILD_LWIP_FLAGS) -DARDUINO=$(RUNTIME_IDE_VERSION)
 RECIPE_C_O_PATTERN          += -DARDUINO_$(BUILD_BOARD) -DARDUINO_ARCH_$(BUILD_ARCH) -DARDUINO_BOARD=$(BUILD_BOARD)
 RECIPE_C_O_PATTERN          += $(INCLUDES) 
 
-RECIPE_CPP_O_PATTERN        := $(COMPILER_PATH)$(COMPILER_CPP_CMD) $(COMPILER_CPREPROCESSOR_FLAGS) $(COMPILER_CPP_FLAGS)
+RECIPE_CPP_O_PATTERN        := $(COMPILER_PATH)/$(COMPILER_CPP_CMD) $(COMPILER_CPREPROCESSOR_FLAGS) $(COMPILER_CPP_FLAGS)
 RECIPE_CPP_O_PATTERN        += -DF_CPU=$(BUILD_F_CPU) $(BUILD_LWIP_FLAGS) -DARDUINO=$(RUNTIME_IDE_VERSION)
 RECIPE_CPP_O_PATTERN        += -DARDUINO_$(BUILD_BOARD) -DARDUINO_ARCH_$(BUILD_ARCH) -DARDUINO_BOARD=$(BUILD_BOARD)
 RECIPE_CPP_O_PATTERN        += $(INCLUDES)
 
 RECIPE_INO_O_PATTERN        := $(RECIPE_CPP_O_PATTERN)  -x c++ -include $(BUILD_CORE_PATH)Arduino.h
 
-RECIPE_AR_PATTERN           := $(COMPILER_PATH)$(COMPILER_AR_CMD) $(COMPILER_AR_FLAGS) $(BUILD_PATH)/arduino.ar 
+RECIPE_AR_PATTERN           := $(COMPILER_PATH)/$(COMPILER_AR_CMD) $(COMPILER_AR_FLAGS) $(BUILD_PATH)/arduino.ar 
 RECIPE_AR_PATTERN           += $(CORE_OBJ_FILES)
 
-RECIPE_C_COMBINE_PATTERN    := $(COMPILER_PATH)$(COMPILER_C_ELF_CMD) $(COMPILER_C_ELF_FLAGS) 
-RECIPE_C_COMBINE_PATTERN    += -o $(BUILD_PATH)$(BUILD_PROJECT_NAME).elf -Wl,--start-group $(OBJECT_FILES) 
-RECIPE_C_COMBINE_PATTERN    += $(BUILD_PATH)arduino.ar 
+RECIPE_C_COMBINE_PATTERN    := $(COMPILER_PATH)/$(COMPILER_C_ELF_CMD) $(COMPILER_C_ELF_FLAGS) 
+RECIPE_C_COMBINE_PATTERN    += -o $(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf -Wl,--start-group $(OBJECT_FILES) 
+RECIPE_C_COMBINE_PATTERN    += $(BUILD_PATH)/arduino.ar 
 RECIPE_C_COMBINE_PATTERN    += $(COMPILER_C_ELF_LIBS) -Wl,--end-group  -L$(BUILD_PATH)
 
-RECIPE_OBJCOPY_HEX_PATTERN  := $(RUNTIME_TOOLS_ESPTOOL_PATH)$(COMPILER_ESPTOOL_CMD) 
+RECIPE_OBJCOPY_HEX_PATTERN  := $(RUNTIME_TOOLS_ESPTOOL_PATH)/$(COMPILER_ESPTOOL_CMD) 
 RECIPE_OBJCOPY_HEX_PATTERN  += -eo $(RUNTIME_PLATFORM_PATH)/bootloaders/eboot/eboot.elf 
-RECIPE_OBJCOPY_HEX_PATTERN  += -bo $(BUILD_PATH)$(BUILD_PROJECT_NAME).bin 
+RECIPE_OBJCOPY_HEX_PATTERN  += -bo $(BUILD_PATH)/$(BUILD_PROJECT_NAME).bin 
 RECIPE_OBJCOPY_HEX_PATTERN  += -bm $(BUILD_FLASH_MODE) -bf $(BUILD_FLASH_FREQ) -bz $(BUILD_FLASH_SIZE) 
-RECIPE_OBJCOPY_HEX_PATTERN  += -bs .text -bp 4096 -ec -eo $(BUILD_PATH)$(BUILD_PROJECT_NAME).elf 
+RECIPE_OBJCOPY_HEX_PATTERN  += -bs .text -bp 4096 -ec -eo $(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf 
 RECIPE_OBJCOPY_HEX_PATTERN  += -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec
 
-RECIPE_SIZE_PATTERN         := $(COMPILER_PATH)$(COMPILER_SIZE_CMD) -A $(BUILD_PATH)$(BUILD_PROJECT_NAME).elf
+RECIPE_SIZE_PATTERN         := $(COMPILER_PATH)/$(COMPILER_SIZE_CMD) -A $(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf
 
-TOOLS_ESPTOOL_UPLOAD_PATTERN := $(PATH)$(CMD) $(UPLOAD_VERBOSE) -cd $(UPLOAD_RESETMETHOD) -cb $(UPLOAD_SPEED) 
-TOOLS_ESPTOOL_UPLOAD_PATTERN += -cp $(SERIAL_PORT) -ca 0x00000 -cf $(BUILD_PATH)$(BUILD_PROJECT_NAME).bin
+TOOLS_ESPTOOL_UPLOAD_PATTERN    := $(PATH)/$(CMD) $(UPLOAD_VERBOSE) -cd $(UPLOAD_RESETMETHOD) -cb $(UPLOAD_SPEED) 
+TOOLS_ESPTOOL_UPLOAD_PATTERN    += -cp $(SERIAL_PORT) -ca 0x00000 -cf $(BUILD_PATH)/$(BUILD_PROJECT_NAME).bin
 
-RECIPE_EXTRA_1_PATTERN      := @$(COMPILER_PATH)$(COMPILER_OBJDUMP_CMD) -D $(BUILD_PATH)$(BUILD_PROJECT_NAME).elf 
-RECIPE_EXTRA_1_PATTERN      += > $(BUILD_PATH)$(BUILD_PROJECT_NAME).list
+RECIPE_EXTRA_1_PATTERN      := @$(COMPILER_PATH)/$(COMPILER_OBJDUMP_CMD) -D $(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf 
+RECIPE_EXTRA_1_PATTERN      += > $(BUILD_PATH)/$(BUILD_PROJECT_NAME).list
 
-RECIPE_EXTRA_2_PATTERN      := @$(COMPILER_PATH)$(COMPILER_READELF_CMD) -S $(BUILD_PATH)$(BUILD_PROJECT_NAME).elf 
-RECIPE_EXTRA_2_PATTERN      += > $(BUILD_PATH)$(BUILD_PROJECT_NAME).section.heads
+RECIPE_EXTRA_2_PATTERN      := @$(COMPILER_PATH)/$(COMPILER_READELF_CMD) -S $(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf 
+RECIPE_EXTRA_2_PATTERN      += > $(BUILD_PATH)/$(BUILD_PROJECT_NAME).section.heads
 
-RECIPE_EXTRA_3_PATTERN      := @$(COMPILER_PATH)$(COMPILER_OBJDUMP_CMD) -D 
+RECIPE_EXTRA_3_PATTERN      := @$(COMPILER_PATH)/$(COMPILER_OBJDUMP_CMD) -D 
 RECIPE_EXTRA_3_PATTERN      += $(RUNTIME_PLATFORM_PATH)/bootloaders/eboot/eboot.elf
-RECIPE_EXTRA_3_PATTERN      += > $(BUILD_PATH)eboot.list
+RECIPE_EXTRA_3_PATTERN      += > $(BUILD_PATH)/eboot.list
 
-RECIPE_EXTRA_4_PATTERN      := @$(COMPILER_PATH)$(COMPILER_READELF_CMD) -S 
+RECIPE_EXTRA_4_PATTERN      := @$(COMPILER_PATH)/$(COMPILER_READELF_CMD) -S 
 RECIPE_EXTRA_4_PATTERN      += $(RUNTIME_PLATFORM_PATH)/bootloaders/eboot/eboot.elf
-RECIPE_EXTRA_4_PATTERN      += > $(BUILD_PATH)eboot.section.heads
+RECIPE_EXTRA_4_PATTERN      += > $(BUILD_PATH)/eboot.section.heads
 
 
 # Rule to build all
-all : $(BUILD_PATH)$(BUILD_PROJECT_NAME).bin 
+all : $(BUILD_PATH)/$(BUILD_PROJECT_NAME).bin 
 
 # Rule to convert the elf format to bin format
-$(BUILD_PATH)$(BUILD_PROJECT_NAME).bin : $(BUILD_PATH)$(BUILD_PROJECT_NAME).elf
+$(BUILD_PATH)/$(BUILD_PROJECT_NAME).bin : $(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf
 	$(RECIPE_OBJCOPY_HEX_PATTERN)
 
 # Rule to link it together
-$(BUILD_PATH)$(BUILD_PROJECT_NAME).elf : $(BUILD_PATH)arduino.ar $(OBJECT_FILES)
+$(BUILD_PATH)/$(BUILD_PROJECT_NAME).elf : $(BUILD_PATH)/arduino.ar $(OBJECT_FILES)
 	$(RECIPE_C_COMBINE_PATTERN)
 
 # Rule to build the arduino.ar archive file
-$(BUILD_PATH)arduino.ar : $(CORE_OBJ_FILES)
+$(BUILD_PATH)/arduino.ar : $(CORE_OBJ_FILES)
 	$(RECIPE_AR_PATTERN)
 
 # Rule to compile the ino file:
@@ -234,7 +251,7 @@ $(BUILD_PATH)/%.S.o : %.S
 
 # Rule to clean up a build:
 clean :
-	$(shell rm -f $(BUILD_PATH)*)
+	$(shell rm -f $(BUILD_PATH)/*)
 
 
 # Rule to calculate the size
@@ -255,7 +272,6 @@ extra :
 	$(RECIPE_EXTRA_2_PATTERN)
 	$(RECIPE_EXTRA_3_PATTERN)
 	$(RECIPE_EXTRA_4_PATTERN)
-
 
 
 
